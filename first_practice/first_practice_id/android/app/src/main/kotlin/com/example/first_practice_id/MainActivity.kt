19fe57ab5979e05f2b7096cb84088a114b22b490
Triboi Dorin <dorintriboi.r@gmail.com>
2024-11-06 14:12:27 +0200

    Removed unecessary projects

--- first_practice/first_practice_id/android/app/src/main/kotlin/com/example/first_practice_id/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.first_practice_id
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- world_time_changer/world_time_changer/android/app/src/main/kotlin/com/example/world_time_changer/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.world_time_changer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
